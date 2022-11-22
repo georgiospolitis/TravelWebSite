9f8b154fba713c25d4b9707b980293f1be6667b1
Georgios Politis <117930721+georgiospolitis@users.noreply.github.com>
2022-11-22 18:04:33 +0200

    Add files via upload

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TravelWebSite.Startup))]
+namespace TravelWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
